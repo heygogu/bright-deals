77d1cd93b4a0f8498c93476ca1e4a6243b67e6dc
Rohit <rohitnarnolia88@gmail.com>
2025-10-07 12:29:43 +0530

    learn about yaml files

--- app/crazy-from/page.tsx
+++ app/crazy-from/page.tsx
@@ -1,5 +1,4 @@
 import { cn } from "@/lib/utils";
-import { div } from "framer-motion/client";
 import React from "react";
 
 export default function BestForm() {
--- app/masonary-grid/page.tsx
+++ app/masonary-grid/page.tsx
@@ -1,5 +1,4 @@
 import { cn } from "@/lib/utils";
-import { div } from "framer-motion/client";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
